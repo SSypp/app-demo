65607e49b44e411235011368c22950e0974c1499
SSypp <2435884205@qq.com>
2019-09-10 00:43:47 +0800

    初始化

---
+++ pages/index/index.vue
@@ -0,0 +1,45 @@
+<template>
+	<view>
+		<view class="uni-list">
+		    <view class="uni-list-cell">
+		        <view class="uni-list-cell-navigate uni-navigate-right">
+		            1
+		        </view>
+		    </view>
+			<view class="uni-list-cell">
+			    <view class="uni-list-cell-navigate uni-navigate-right">
+			        2
+			    </view>
+			</view>
+			<view class="uni-list-cell">
+			    <view class="uni-list-cell-navigate uni-navigate-right">
+			        3
+			    </view>
+			</view>
+			<span class="iconfont icon-xiaoxi" style='color: #1AAD19;font-size: 30px;'></span>
+			<view>
+				<navigator url="../mine/index">点击</navigator>
+			</view>
+		</view>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				
+			}
+		},
+		onLoad() {
+
+		},
+		methods: {
+
+		}
+	}
+</script>
+
+<style>
+	
+</style>
---
+++ pages/mine/index.vue
@@ -0,0 +1,9 @@
+<template>
+	<view class="animated bounce">hello world</view>
+</template>
+
+<script>
+</script>
+
+<style>
+</style>
